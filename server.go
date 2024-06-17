28f280f64915aa4af9ec361380b1e0122e4dd155
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-06-17 17:19:15 +0100

    Update server.go

--- server.go
+++ server.go
@@ -1,1 +1,22 @@
+func main() {
+    router := mux.NewRouter()
+    router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
 
+      
+        // an example API handler
+        json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+    })
+
+    spa := router.PathPrefix("/").Handler(http.StripPrefix("/",http.FileServer(http.Dir("./static/")
+    router.PathPrefix("/").Handler(spa)
+
+    srv := &http.Server{
+        Handler: router,
+        Addr:    "127.0.0.1:8000",
+        // Good practice: enforce timeouts for servers you create!
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout:  15 * time.Second,
+    }
+
+    log.Fatal(srv.ListenAndServe())
+}
